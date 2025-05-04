9d5cdd3415837475f0d9b0c756f7ad01abe10cd6
dselokela023 <dselokela023@student.wethinkcode.co.za>
2025-05-04 04:51:09 +0200

    added functions on the context

--- packages/react-app/contexts/useWeb3.ts
+++ packages/react-app/contexts/useWeb3.ts
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import StableTokenABI from "./cusd-abi.json";
 import StokvelNFT from "./StockvelNFT.json"
+import Treasury from "./Treasury.json";
 import StokvelTreasury from "./Treasury.json"
 import { BEFTokenAddress, StockvelNFTAddress, TreasuryAddress } from "../constants";
 
@@ -104,7 +105,26 @@ export const useWeb3 = () => {
 
         return res;
 
+    }
 
+    const hasJoined = async () => {
+        
+        let celoWalletClient = createWalletClient({
+            transport: custom(window.ethereum),
+            chain: celoAlfajores,
+        });
+        
+        let [address] = await celoWalletClient.getAddresses();
+
+        const res = await celoPublicClient.readContract({
+            address: StockvelNFTAddress,
+            abi: StokvelNFT.abi,
+            functionName: 'hasJoined',
+            args: [u_address],
+            account: address,
+          });
+
+          return res;
     }
 
     const increaseStableAllowance = async (amount: number) => {
@@ -141,6 +161,77 @@ export const useWeb3 = () => {
 
         return res;
     };
+    
+    const contribute = async (tokenId: number) =>{
+        
+        let walletClient = createWalletClient({
+            transport: custom(window.ethereum),
+            chain: celoAlfajores,
+        });
+
+        let [address] = await walletClient.getAddresses();
+
+        try {
+            const res = await walletClient.writeContract({
+                address: TreasuryAddress,
+                abi: Treasury.abi,
+                functionName: 'contribute',
+                args: [tokenId],
+                account: address,
+              })
+
+              return res;
+
+        } catch (e) {
+            alert(e);
+        }
+    }
+
+    const getTreasuryBalances = async () =>{
+        let celoWalletClient = createWalletClient({
+            transport: custom(window.ethereum),
+            chain: celoAlfajores,
+        });
+        
+        let [address] = await celoWalletClient.getAddresses();
+
+        const savings = await celoPublicClient.readContract({
+            address: TreasuryAddress,
+            abi: Treasury.abi,
+            functionName: 'savingsBalance',
+            account: u_address,
+          });
+
+        
+          const defiBalance = await celoPublicClient.readContract({
+            address: TreasuryAddress,
+            abi: Treasury.abi,
+            functionName: 'defiBalance',
+            account: u_address,
+          });
+
+        
+
+          return {savings, defiBalance};
+    }
+
+    const memberData = async () => {
+        let celoWalletClient = createWalletClient({
+            transport: custom(window.ethereum),
+            chain: celoAlfajores,
+        });
+        
+        let [address] = await celoWalletClient.getAddresses();
+
+        const Mdata = await celoPublicClient.readContract({
+            address: TreasuryAddress,
+            abi: Treasury.abi,
+            functionName: 'memberData',
+            account: u_address,
+          });
+
+        return Mdata;
+    }
 
     return {
         u_address,
@@ -150,5 +241,14 @@ export const useWeb3 = () => {
         getStableAllowance,
         increaseStableAllowance,
         mintTokens,
+        hasJoined,
+        contribute,
+        getTreasuryBalances,
+        memberData
+
     };
 };
+function async() {
+    throw new Error("Function not implemented.");
+}
+
